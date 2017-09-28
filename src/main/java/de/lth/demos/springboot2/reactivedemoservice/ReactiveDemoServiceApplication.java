5f53d158cf85946ce216c0c4c75bbf3de1454c9d
Lars Thielmann <lars.thielmann@gmail.com>
2017-09-28 19:45:51 +0200

    Initial commit

---
+++ src/main/java/de/lth/demos/springboot2/reactivedemoservice/ReactiveDemoServiceApplication.java
@@ -0,0 +1,12 @@
+package de.lth.demos.springboot2.reactivedemoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveDemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveDemoServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/de/lth/demos/springboot2/reactivedemoservice/ReactiveDemoServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package de.lth.demos.springboot2.reactivedemoservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveDemoServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
